e1cc553cd6dd83bc8b4897e5a4f3e6555d5af281
Mikie <mgable1995@gmail.com>
2021-04-22 16:12:01 -0400

    Initial Commit

---
+++ Breweries/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Breweries
+//
+//  Created by Michael Gable on 4/22/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
